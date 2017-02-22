42cf37329bef0b68650f6aa2379f6b4c64c833a1
nolofinwe <filipdalorenzo@gmail.com>
2017-02-22 17:59:51 +0100

    add CSV writer

---
+++ app/src/main/java/com/filip/tomasovych/keystrokeauthentication/app/util/CSVWriter.java
@@ -0,0 +1,66 @@
+package com.filip.tomasovych.keystrokeauthentication.app.util;
+
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+/**
+ * Created by nolofinwe on 22/02/17.
+ */
+
+public class CSVWriter {
+
+    private static final char DEFAULT_SEPARATOR = ',';
+
+    public static void writeLine(FileOutputStream w, List<String> values) throws IOException {
+        writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+    }
+
+    public static void writeLine(FileOutputStream w, List<String> values, char separators) throws IOException {
+        writeLine(w, values, separators, ' ');
+    }
+
+    //https://tools.ietf.org/html/rfc4180
+    private static String followCVSformat(String value) {
+
+        String result = value;
+        if (result.contains("\"")) {
+            result = result.replace("\"", "\"\"");
+        }
+        return result;
+
+    }
+
+    public static void writeLine(FileOutputStream w, List<String> values, char separators, char customQuote) throws IOException {
+
+        boolean first = true;
+
+        //default customQuote is empty
+
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String value : values) {
+            if (!first) {
+                sb.append(separators);
+            }
+            if (customQuote == ' ') {
+                sb.append(followCVSformat(value));
+            } else {
+                sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
+            }
+
+            first = false;
+        }
+        sb.append("\n");
+        w.write(sb.toString().getBytes());
+
+
+    }
+
+}
--- app/src/main/java/com/filip/tomasovych/keystrokeauthentication/app/util/KeyController.java
+++ app/src/main/java/com/filip/tomasovych/keystrokeauthentication/app/util/KeyController.java
@@ -7,6 +7,12 @@ import com.filip.tomasovych.keystrokeauthentication.app.model.KeyBuffer;
 import com.filip.tomasovych.keystrokeauthentication.app.model.KeyObject;
 import com.filip.tomasovych.keystrokeauthentication.app.model.User;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by nolofinwe on 14.10.2016.
  */
@@ -16,11 +22,13 @@ public class KeyController {
     private final static String TAG = KeyController.class.getSimpleName();
 
     private DbHelper mDbHelper;
+    private Context mContext;
     private User mUser;
 
 
     public KeyController(Context context, User user) {
         mDbHelper = DbHelper.getInstance(context);
+        mContext = context;
         mUser = user;
     }
 
@@ -40,9 +48,63 @@ public class KeyController {
             }
         }
 
+        saveCSV(keyBuffer, state);
         keyBuffer.clear();
 
         return true;
+    }
+
+    private boolean saveCSV(KeyBuffer keyBuffer, int state) {
+        String csvFile = "TESTFIL.csv";
+        boolean ex = fileExists(csvFile);
+        FileOutputStream outputStream;
+
+
+        int bufferSize = keyBuffer.getSize();
+        List<String> columns = new ArrayList<>();
+
+        for (int i = 0; i < bufferSize; i++) {
+            columns.add("xCoordPress" + i);
+            columns.add("yCoordPress" + i);
+            columns.add("xCoordRelease" + i);
+            columns.add("yCoordRelease" + i);
+            columns.add("pressPressure" + i);
+            columns.add("releasePressure" + i);
+            columns.add("holdTime" + i);
+            columns.add("offSet" + i);
+
+        }
+
+        for (int i = 0; i < bufferSize - 1; i++) {
+            columns.add("PPTime" + i);
+            columns.add("UDTime" + i);
+        }
+
+
+
+        try {
+            outputStream = mContext.openFileOutput(csvFile, Context.MODE_PRIVATE);
+
+
+            if (!ex)
+                CSVWriter.writeLine(outputStream, columns);
+
+
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+
+        return true;
+    }
+
+
+    private boolean fileExists(String fname){
+        File file = mContext.getFileStreamPath(fname);
+        return file.exists();
     }
 
 }
